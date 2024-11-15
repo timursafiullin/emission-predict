7461c1850608c3d9d4bbd333725f751b44fecd68
TimurSafiullin <safiullintb@yandex.ru>
2024-11-15 17:13:07 +0300

    Delete unusing class Font

--- lib/graph_lib/fltk_design.h
+++ lib/graph_lib/fltk_design.h
@@ -13,30 +13,4 @@ namespace COLORS
     const auto BRIGHT_BLUE = fl_rgb_color(7, 118, 245);
 };
 
-class Font
-{
-public:
-  enum Font_type
-  {
-    helvetica = FL_HELVETICA,
-    helvetica_bold = FL_HELVETICA_BOLD,
-    helvetica_italic = FL_HELVETICA_ITALIC,
-    helvetica_bold_italic = FL_HELVETICA_BOLD_ITALIC,
-    courier = FL_COURIER,
-    courier_bold = FL_COURIER_BOLD,
-    courier_italic = FL_COURIER_ITALIC,
-    courier_bold_italic = FL_COURIER_BOLD_ITALIC,
-    symbol = FL_SYMBOL,
-  };
-
-  Font(Font_type ff) : f{ff} {}
-
-  Font(int ff) : f{ff} {}
-
-  int as_int () const { return f; }
-
-private:
-  int f;
-};
-
 #endif // FL_DESIGN
