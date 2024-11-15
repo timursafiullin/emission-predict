12a94aeeb53d20dc83ad3af10c5a3a0dd5ef8f12
TimurSafiullin <safiullintb@yandex.ru>
2024-11-15 18:02:45 +0300

    Corrected Window.h and Window.cpp

--- lib/graph_lib/Window.cpp
+++ lib/graph_lib/Window.cpp
@@ -1,11 +1,67 @@
 #include "fltk.h"
 #include "Objects.h"
+#include "Window.h"
 
-int create_window(unsigned int width, unsigned int height, std::string title = "Unnamed")
+ProgrammWindow::ProgrammWindow(int w, int h, const std::string& title) : Fl_Window(w, h) 
 {
-  Fl_Window *window = new Fl_Window(width, height, title.c_str());
-  window->color(COLORS::LIGHT_GRAY);
-  window->end();
-  window->show();
-  return Fl::run();
+  label(title.c_str());
+};
+
+void ProgrammWindow::draw()
+{
+  Fl_Window::draw();
+  LabelsList labels_list
+  (
+    std::initializer_list<Labels>
+    {
+      Labels(std::initializer_list<std::string>
+      {
+        "Parameters", "Vehicle Type", "Fuel Type",
+        "Engine Size", "Age of Vehicle", "Mileage",
+        "Speed", "Acceleration", "Road Type",
+        "Traffic Conditions", "Temperature", "Humidity",
+        "Wind Speed", "Air Pressure"
+      }, context_column, 0),
+      Labels(std::initializer_list<std::string>
+      {
+        "Values"
+      }, context_column, 1)
+    }
+  );
+
+  Table* tbl = new Table(561, 21, 260, 406, 2, 14,
+    COLORS::GRAY, COLORS::DARK_GRAY
+  );
+
+  tbl->set_label(labels_list);
+  
+}
+
+void ProgrammWindow::show() 
+{
+  Fl_Window::show();
+}
+
+void ProgrammWindow::hide() 
+{
+  Fl_Window::hide();
+}
+
+void ProgrammWindow::resize(int x, int y, int w, int h) 
+{
+  Fl_Window::resize(x, y, w, h);
+}
+
+void ProgrammWindow::display() 
+{
+  show();
+  Fl::run();
+}
+
+int create_window(unsigned int width, unsigned int height, std::string title, Fl_Color background_color = FL_WHITE)
+{
+  ProgrammWindow* window = new ProgrammWindow(width, height, title);
+  window->color(background_color);
+  window->display();
+  return 0;
 }
--- lib/graph_lib/Window.h
+++ lib/graph_lib/Window.h
@@ -8,71 +8,17 @@
 class ProgrammWindow : public Fl_Window
 {
 public:
-  ProgrammWindow(int w, int h, const std::string& title) : Fl_Window(w, h) 
-  {
-    label(title.c_str());
-  }
-
+  ProgrammWindow(int w, int h, const std::string& title);
   virtual ~ProgrammWindow() {}
 
-  void draw() override 
-  {
-    Fl_Window::draw();
+  void draw() override;
 
-    LabelsList labels_list
-    (
-      std::initializer_list<Labels>
-      {
-        Labels(std::initializer_list<std::string>
-        {
-          "Parameters", "Vehicle Type", "Fuel Type",
-          "Engine Size", "Age of Vehicle", "Mileage",
-          "Speed", "Acceleration", "Road Type",
-          "Traffic Conditions", "Temperature", "Humidity",
-          "Wind Speed", "Air Pressure"
-        }, context_column, 0),
-        Labels(std::initializer_list<std::string>
-        {
-          "Values"
-        }, context_column, 1)
-      }
-    );
-
-    Table* tbl = new Table(561, 21, 260, 406, 2, 14,
-      COLORS::GRAY, COLORS::DARK_GRAY
-    );
-
-    tbl->set_label(labels_list);
-    
-  }
-
-  void show() 
-  {
-    Fl_Window::show();
-  }
-
-  void hide() 
-  {
-    Fl_Window::hide();
-  }
-
-  void resize(int x, int y, int w, int h) 
-  {
-    Fl_Window::resize(x, y, w, h);
-  }
-
-  void display() 
-  {
-    show();
-    Fl::run();
-  }
+  void show();
+  void hide();
+  void resize(int x, int y, int w, int h);
+  void display();
 };
 
-int create_window(unsigned int width, unsigned int height, std::string title, Fl_Color background_color = FL_WHITE) {
-  ProgrammWindow* window = new ProgrammWindow(width, height, title);
-  window->color(background_color);
-  window->display();
-  return 0;
-}
+int create_window(unsigned int width, unsigned int height, std::string title, Fl_Color background_color);
 
 #endif // WINDOW_H
