1c090ed339cf3c43cd8322935309de93aab6187f
TimurSafiullin <safiullintb@yandex.ru>
2024-11-23 10:32:46 +0300

    Delete own graph_lib, delete unused comments

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -1,28 +1,6 @@
 #include "gui.h"
 #include <exception>
 
-
-// void ProgramWindow::draw()
-// {
-//   Fl_Window::draw();
-// 
-//   Table* parameters_table = new Table
-//   (
-//     table_x, table_y,
-//     table_width, table_height,
-//     table_cols, table_rows,
-//     COLORS::LIGHT_GRAY, COLORS::LIGHT_GRAY
-//   );
-// 
-//   parameters_table->set_label(labels_list);
-// 
-// }
-
-
-// void run_gui(unsigned int width, unsigned int height, std::string title, Fl_Color background_color = FL_WHITE)
-// {
-// }
-
 int main()
 try
 {
