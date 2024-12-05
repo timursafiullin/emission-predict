1f1f35d4d00c26af86a43be35b867189c8eaeca6
Руслан <rusmipt@gmail.com>
2024-12-05 19:06:16 +0300

    add member-func to DatasetCell

--- src/dataset/dataset_reader.cpp
+++ src/dataset/dataset_reader.cpp
@@ -4,6 +4,75 @@
 CsvReader csv_reader;
 std::string filename = "../dataset/vehicle_emission_dataset.csv";
 
+std::vector<long double> DatasetCell::normalise_data()
+{
+    std::vector<long double> vec(19);
+    vec[0] = turn_vehicle_type_to_double(vehicle_type);
+    vec[1] = turn_fuel_type_to_double(fuel_type);
+    vec[2] = engine_size;
+    vec[3] = age_of_vehicle;
+    vec[4] = mileage;
+    vec[5] = speed;
+    vec[6] = acceleration;
+    vec[7] = turn_road_type_to_double(road_type);
+    vec[8] = turn_traffic_conditions_to_double(traffic_conditions);
+    vec[9] = temperature;
+    vec[10] = humidity;
+    vec[11] = wind_speed;
+    vec[12] = air_pressure;
+    vec[13] = 1e1 * CO2_emissions;
+    vec[14] = 1e2 * NOx_emissions;
+    vec[15] = 1e3 * PM_emissions;
+    vec[16] = 1e3 * VOC_emissions;
+    vec[17] = 1e3 * SO2_emissions;
+
+    return vec;
+}
+
+long double DatasetCell::turn_vehicle_type_to_double(std::string vehicle)
+{
+    //Bus, Car, Truck, Motorcycle
+    if (vehicle == "Truck")
+        return 1000;
+    if (vehicle == "Bus")
+        return 666;
+    if (vehicle == "Car")
+        return 333;
+    return 0;
+}
+
+long double DatasetCell::turn_fuel_type_to_double(std::string fuel)
+{
+    //Electric, Hybrid, Diesel, Petrol
+    if (fuel == "Petrol")
+        return 1000;
+    if (fuel == "Diesel")
+        return 666;
+    if (fuel == "Hybrid")
+        return 333;
+    return 0;
+}
+
+long double DatasetCell::turn_road_type_to_double(std::string road)
+{
+    //City, Highway, Rural
+    if (road == "City")
+        return 1000;
+    if (road == "Highway")
+        return 500;
+    return 0;
+}
+
+long double DatasetCell::turn_traffic_conditions_to_double(std::string traffic)
+{
+    //Moderate Heavy Free flow
+    if (traffic == "Heavy")
+        return 1000;
+    if (traffic == "Moderate")
+        return 500;
+    return 0;
+}
+
 DatasetCell get_cell()
 {
     if (!csv_reader.is_open())
